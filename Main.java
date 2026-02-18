ee7ab5942363dc665899f6dfa0d5aa6eebd706f9
AKCorderoRicaldi2251 <ac2251@sussex.ac.uk>
2026-02-18 20:21:22 +0000

    a

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
 import java.lang.Math;
 
 public class Main {
+    /**
+     * Evaluates a mathematical expression written in Reverse Polish Notation (RPN).
+     *
+     * The method processes tokens from left to right
+     * @param expression The RPN expression as a space-separated string.
+     * @return The evaluated result rounded to 9 decimal places.
+     *
+     * @throws IllegalArgumentException if:
+     *         - The expression is null or empty
+     *         - There are invalid tokens
+     *         - There are insufficient operands
+     *         - There are too many operands remaining
+     *
+     * @throws ArithmeticException if division by zero occurs.
+     */
 
     public double evaluateStr(String expression){
         Stack<Double> stack = new LinkedStack<>();
@@ -60,13 +75,30 @@ public class Main {
 
         }
         double result = stack.pop();
-        return Math.round(result * 1000000000.0) / 1000000000.0;
+        return Math.round(result * 1000000000.0) / 1000000000.0;  // floating point fix
     }
+    /**
+     * Determines whether a given string token is a valid operator.
+     *
+     * @param s The token to check.
+     * @return true if the token is one of the supported operators,
+     *         otherwise false.
+     */
 
     public boolean isOperator(String s){
         return s.equals("+") || s.equals("-") || s.equals("^")
                 || s.equals("*") || s.equals("/");
     }
+    /**
+     * Determines whether a given string token represents a valid number.
+     *
+     * The method attempts to parse the string as a double.
+     * If parsing succeeds, the token is considered numeric.
+     *
+     * @param s The token to check.
+     * @return true if the token can be parsed as a double,
+     *         otherwise false.
+     */
 
     public static boolean isNumber(String s){
         try{
